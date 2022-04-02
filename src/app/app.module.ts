9b13357a760c33823f737f23fbf38cabd82217a3
p0zzle <leon.collazo.are@gmail.com>
2022-04-02 11:08:12 -0300

    commit on 'main'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { SocialsHeaderComponent } from './components/socials-header/socials-header.component';
+import { faCoffee } from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SocialsHeaderComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+  constructor(library: FaIconLibrary) {
+    // FortAwesome library implementation, as per their Git info.
+    library.addIcons(faCoffee)
+  }
+ }
---
+++ src/app/components/socials-header/socials-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-socials-header',
+  templateUrl: './socials-header.component.html',
+  styleUrls: ['./socials-header.component.css']
+})
+export class SocialsHeaderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
